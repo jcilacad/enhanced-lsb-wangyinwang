4b1b8d765346a6cf36c245196c18c73560d3c53b
JCILACAD <johnchristopherilacad27@gmail.com>
2024-02-18 18:45:30 +0800

    Refactored datasets. Added PSNR results.

--- HiddenImageTest.py
+++ HiddenImageTest.py
@@ -3,6 +3,8 @@
 # Equals 0: (Equals) Difference =  0.0  == Carrier Image Dimensions - ( 200 ,  249 ) === Hidden Image Dimension - ( 75 ,  83 )
 # Positive 1: (Positive) Difference =  1.0  == Carrier Image Dimensions - ( 201 ,  209 ) === Hidden Image Dimension - ( 59 ,  89 )
 # Negative 1: (Negative) Difference =  -1.0  == Carrier Image Dimensions - ( 201 ,  215 ) === Hidden Image Dimension - ( 73 ,  74 )
+
+# Negative 3: (Negative) Difference = -3.0 == Carrier Image Dimensions - ( 201 , 285 ) === Hidden Image Dimension - ( 77 , 93 )
 # ===========================================================================================================================================
 # Mid Resolution
 # Equals 0: (Equals) Difference =  0.0  == Carrier Image Dimensions - ( 600 ,  630 ) === Hidden Image Dimension - ( 210 ,  225 )
@@ -21,19 +23,19 @@
 
 # ===========================================================================================================================================
 
-for x in range(1100, 1200):
-    for y in range(1100, 1200):
+for x in range(200, 300):
+    for y in range(200, 300):
         quotient = (x * y * 8) / 8
-        for a in range(400, 450):
-            for b in range(400, 450):
+        for a in range(50, 100):
+            for b in range(50, 100):
                 difference = quotient - (a * b * 8)
                 if difference == 0:
                     print("(Equals) Difference = ", difference, " == Carrier Image Dimensions - (", x, ", ", y,
                           ") === Hidden Image Dimension - (", a, ", ", b, ")")
-                elif 5 > difference > 0:
+                elif 11 > difference > 0:
                     print("(Positive) Difference = ", difference, " == Carrier Image Dimensions - (", x, ", ", y,
                           ") === Hidden Image Dimension - (", a, ", ", b, ")")
-                elif -5 < difference < 0:
+                elif -11 < difference < 0:
                     print("(Negative) Difference = ", difference, " == Carrier Image Dimensions - (", x, ", ", y,
                           ") === Hidden Image Dimension - (", a, ", ", b, ")")
 print("Done")
